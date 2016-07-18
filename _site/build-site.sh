5dd911b11888daa9562726cbe31af43dd4014b65
Michael Drogalis <mjd3089@rit.edu>
2016-07-17 18:09:47 -0700

    0.9.8 doc fix.

--- _site/build-site.sh
+++ _site/build-site.sh
@@ -9,11 +9,15 @@ if [[ "$#" -ne 2 ]]; then
     echo "Usage: $0 <git sha or tag of Onyx core> <onyx version>"
     echo "Example: $0 0.8.4 0.8.4"
 else
+    # Ensure we're in the project's dir before we start
+    cd "$(dirname "$0")"
+
     # Build user guide
     rm -rf onyx
     git clone https://github.com/onyx-platform/onyx.git
     git -C onyx checkout $1
 
+    mkdir -p docs/user-guide/latest
     mkdir -p docs/user-guide/$2
     rm -rf docs/user-guide/$2/*
     cp -R onyx/doc/user-guide/* docs/user-guide/$2/
@@ -24,6 +28,8 @@ else
     mkdir -p docs/api/$2/
     rm -rf docs/api/$2/*
     cp -R onyx/doc/api/* docs/api/$2/
+
+    mkdir -p docs/api/latest/
     rm -rf docs/api/latest/*
     cp -R onyx/doc/api/* docs/api/latest/
 
@@ -35,12 +41,15 @@ else
     rm onyx-cheat-sheet/resources/public/js/app.js || true
     mkdir -p docs/cheat-sheet/$2
     rm -rf docs/cheat-sheet/$2/*
-    cd onyx-cheat-sheet && lein update-dependency org.onyxplatform/onyx $2 && lein with-profile -dev,+uberjar cljsbuild once
+    cd onyx-cheat-sheet && lein update-dependency org.onyxplatform/onyx $1 && lein with-profile -dev,+uberjar cljsbuild once
     cd ..
+
+    echo "Working dir before copy " $PWD 
     cp onyx-cheat-sheet/resources/public/js/app.js docs/cheat-sheet/$2
     cp onyx-cheat-sheet/resources/public/css/style.css css/cheat-sheet-style.css
     cp onyx-cheat-sheet/resources/index.html docs/cheat-sheet/$2
 
+    mkdir -p docs/user-guide/latest
     rm -rf docs/cheat-sheet/latest/*
     cp onyx-cheat-sheet/resources/public/js/app.js docs/cheat-sheet/latest
     cp onyx-cheat-sheet/resources/index.html docs/cheat-sheet/latest
@@ -51,5 +60,6 @@ else
 
     # Update Onyx version
     sed -i.bak "s/.*onyx_version.*/onyx_version: $2/g" _config.yml
+    git add _config.yml
     rm _config.yml.bak
 fi
